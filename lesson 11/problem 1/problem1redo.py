3f0a8d489dbdfe3e40a713abaa01cb1069696eba
Kruddyy <43756292+Kruddyy@users.noreply.github.com>
2018-12-21 10:11:55 -0500

    Add files via upload

---
+++ lesson 11/problem 1/problem1redo.py
@@ -0,0 +1,11 @@
+print('-'*65)
+print('Iz iT BrIcK BoT?')
+print()
+Temperature = input('Temperature in Fahrenheit: ')
+Temperature = int(Temperature)
+
+if Temperature <= 40:
+	print('ItZ bRIck, wear ya jacket.' )
+else:
+	print('ItZ not bRIck, enjoy ya day.')
+print('-'*65)
---
+++ lesson 11/problem 2/problem2.py
@@ -0,0 +1,10 @@
+print('-'*65)
+name = input('Student Name:?' )
+print('Student Name:' + name )
+grade= input('Grade:? ')
+print('Grade:' + grade ) 
+
+Narrative = input('Would you like a good or bad narrative for this student?')
+
+if Narrative == 'Yes':
+	print(name +, 'your final grade in Ap computuer science is a' + grade + '. You have excelled in all componets of the class! I wish ou continued success in the next semester of Ap computer science!')
